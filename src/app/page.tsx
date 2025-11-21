379180bd5f7ae446ccf41f278dfadbecf763c17a
asta <asta@flash.co>
2025-11-21 07:31:46 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
-import { Metadata } from "next";
-import App from "./app";
-import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
-import { getMiniAppEmbedMetadata } from "~/lib/utils";
+"use client";
 
-export const revalidate = 300;
+import TicTacToe from './app';
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: APP_NAME,
-    openGraph: {
-      title: APP_NAME,
-      description: APP_DESCRIPTION,
-      images: [APP_OG_IMAGE_URL],
-    },
-    other: {
-      "fc:frame": JSON.stringify(getMiniAppEmbedMetadata()),
-    },
-  };
-}
-export const metadata = {
-  other: {
-    "fc:frame": "vNext",
-    "fc:frame:image": "https://tictactoe-three-peach.vercel.app/frame.png",
-  }
-};
-
-
-export default function Home() {
-  return (<App />);
+export default function Page() {
+  return <TicTacToe />;
 }
