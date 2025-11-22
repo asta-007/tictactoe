bdfe0051246eb5a3e7a634100c07f4e95510257c
asta <asta@flash.co>
2025-11-22 10:22:15 +0530

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019aa9e5-1de3-cb07-edc4-6271fb37c260',
+        permanent: false, // 307 temporary redirect
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
